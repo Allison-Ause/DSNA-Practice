6244b65dabcb571ee55f6e55ae5d03b0059a8ec8
Allison Ause <allison.ause@gmail.com>
2022-11-15 16:47:07 -0800

    removed skips from tests

--- day-01/index.test.js
+++ day-01/index.test.js
@@ -13,39 +13,39 @@ const {
 
 describe('primitive code challenges', () => {
   describe('reverseSentence', () => {
-    it.skip('should reverse a sentence', () => {
+    it('should reverse a sentence', () => {
       expect(reverseSentence('alchemy rocks gold')).toBe('ymehcla skcor dlog');
     });
   });
 
   describe('addPunctuation', () => {
-    it.skip('takes punctuation marks and returns function that takes a string and adds the punctuation mark', () => {
+    it('takes punctuation marks and returns function that takes a string and adds the punctuation mark', () => {
       const addExcitement = addPunctuation('!!!');
       expect(addExcitement('Hiya, Friend')).toBe('Hiya, Friend!!!');
     });
   });
 
   describe('addFirst', () => {
-    it.skip('takes an element and returns a function which takes an array and returns new array with element at index 0', () => {
+    it('takes an element and returns a function which takes an array and returns new array with element at index 0', () => {
       const addOrange = addFirst('orange');
       expect(addOrange(['rose', 'honey'])).toEqual(['orange', 'rose', 'honey']);
     });
   });
 
   describe('titleCase', () => {
-    it.skip('capitalizes the first letter of every word in a provided sentence', () => {
+    it('capitalizes the first letter of every word in a provided sentence', () => {
       expect(titleCase('I love my doggo.')).toBe('I Love My Doggo.');
     });
   });
 
   describe('evenOrOddish', () => {
-    it.skip('sums the numbers constituting a string and determines if its even or odd', () => {
+    it('sums the numbers constituting a string and determines if its even or odd', () => {
       expect(evenOrOddish('523')).toBe('Evenish');
     });
   });
 
   describe('fizzbuzz', () => {
-    it.skip('divisible by 3 = fizz, divisible by 5 = buzz, divisible by both = fizzbuzz', () => {
+    it('divisible by 3 = fizz, divisible by 5 = buzz, divisible by both = fizzbuzz', () => {
       expect(fizzBuzz(20)).toEqual([
         1,
         2,
@@ -72,13 +72,13 @@ describe('primitive code challenges', () => {
   });
 
   describe('multiplesOfN', () => {
-    it.skip('returns an array of numbers divisible by the n parameter', () => {
+    it('returns an array of numbers divisible by the n parameter', () => {
       expect(multiplesOfN(10)).toEqual([10, 20, 30, 40, 50]);
     });
   });
 
   describe('convert', () => {
-    it.skip('converts roman numeral to numeric value', () => {
+    it('converts roman numeral to numeric value', () => {
       expect(convert('IV')).toBe(4);
     });
   });
